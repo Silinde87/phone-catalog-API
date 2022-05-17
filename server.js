f9884d26a260020c75329ac095b9f6bd04932dfc
Silinde87 <paurodriguezmolina@gmail.com>
2022-05-17 19:48:51 +0200

    feat: configure entry point, db, cors and middleware

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+
+// DB config
+require('./configs/db.config');
+
+const app = express();
+
+// Middleware config
+require('./configs/middleware.config')(app);
+require('./configs/cors.config')(app);
+
+app.use((req, res, next) => {
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'OPTIONS, PUT, POST, DELETE, GET, PATCH');
+		return res.status(200).json({});
+	}
+	next();
+});
+
+module.exports = app;
