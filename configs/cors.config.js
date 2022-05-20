30a1c1f4d78cf31d33f129f9924679a4b76a899f
Pau Rodriguez <paurodriguezmolina@gmail.com>
2022-05-20 09:11:58 +0200

    fix: restore headers

--- configs/cors.config.js
+++ configs/cors.config.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
 	app.use(
 		cors({
 			credentials: true,
-			origin: ['http://localhost:3000', 'https://phone-catalog-sandy.vercel.app/', 'https://*.vercel.app'],
+			origin: ['http://localhost:3000', 'https://phone-catalog-sandy.vercel.app', 'https://*.vercel.app'],
 		})
 	);
 };
--- server.js
+++ server.js
@@ -10,6 +10,18 @@ const app = express();
 require('./configs/middleware.config')(app);
 require('./configs/cors.config')(app);
 
+app.use((req, res, next) => {
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'OPTIONS, PUT, POST, DELETE, GET, PATCH');
+		return res.status(200).json({});
+	}
+	next();
+});
+
 const phoneRouter = require('./routes/phone.routes');
 app.use('/api', phoneRouter);
 
